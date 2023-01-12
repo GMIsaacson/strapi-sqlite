33839db2121e61df2db20619e17e0956cc1315c1
unknown <iceark5@yahoo.com>
2023-01-11 18:40:00 -0600

    first commit

--- config/server.js
+++ config/server.js
@@ -5,3 +5,11 @@ module.exports = ({ env }) => ({
     keys: env.array('APP_KEYS')
   },
 });
+
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: ['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', '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']
+  },
+});
