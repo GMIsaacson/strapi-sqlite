7c5de0846d3f4f73baaadf95edf8e9de6571d3e0
unknown <iceark5@yahoo.com>
2023-01-11 21:20:36 -0600

    fixed server 1

--- config/server.js
+++ config/server.js
@@ -7,8 +7,9 @@ module.exports = ({ env }) => ({
 });
 
 module.exports = ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL"),
-  dirs: {
-    public: "/data/public"
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS')
   },
 });
