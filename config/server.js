46e91ce0063c064e6b835f87964e77079a5831b1
Isaac G Mwithiga <iceark5@yahoo.com>
2023-01-11 21:29:17 -0600

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,12 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS')
-  },
-});
-
-module.exports = ({ env }) => ({
   url: env("RENDER_EXTERNAL_URL"),
   dirs: {
     public: "/data/public"
